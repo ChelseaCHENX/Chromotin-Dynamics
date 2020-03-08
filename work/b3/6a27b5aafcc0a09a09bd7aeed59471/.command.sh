ff935385756931ae73ca4c5ee0309f5a60cb2ed4
Fangyuan <chelseax488@gmail.com>
2020-03-08 14:48:59 -0400

    Mar 8

--- work/b3/6a27b5aafcc0a09a09bd7aeed59471/.command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ue
-TASK_TMP_DIR=$(mktemp -d -p null distiller.tmp.XXXXXXXXXX)
-touch SRR6493702.lane1.GRCh38.0.bam 
-bwa mem -t 1 -v 3 -SP Homo_sapiens.GRCh38.dna.primary_assembly.fa SRR6493702_1.fastq.gz SRR6493702_2.fastq.gz         | tee >(samtools view -bS > SRR6493702.lane1.GRCh38.0.bam)         | pairtools parse                --add-columns mapq             -c hg38.chrom.sizes             | pairtools sort --nproc 1                              -o SRR6493702.lane1.GRCh38.0.pairsam.gz                              --tmpdir $TASK_TMP_DIR             | cat
-
-rm -rf $TASK_TMP_DIR
